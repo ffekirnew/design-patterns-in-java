e05e50b140cc960842c30978d07beb1ea9211d8a
ffekirnew <ffekirnew0808@gmail.com>
2024-01-17 21:48:09 +0300

    feat: Instantiate the main code

---
+++ src/main/java/me/fikernewbirhanu/designpatterns/App.java
@@ -0,0 +1,10 @@
+package me.fikernewbirhanu.designpatterns;
+
+import me.fikernewbirhanu.designpatterns.iterator.Main;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    Main.main();
+  }
+}
